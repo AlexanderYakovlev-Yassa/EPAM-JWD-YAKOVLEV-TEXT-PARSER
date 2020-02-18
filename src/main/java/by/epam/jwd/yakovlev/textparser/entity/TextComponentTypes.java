3532b674e1326fe9d31b0f755ebfb9bf67fb5bc4
Alexander Yakovlev <sashayassa@gmail.com>
2020-02-18 09:49:23 +0300

    Add TextComponentType

---
+++ src/main/java/by/epam/jwd/yakovlev/textparser/entity/TextComponentTypes.java
@@ -0,0 +1,15 @@
+package by.epam.jwd.yakovlev.textparser.entity;
+
+public enum TextComponentTypes {
+
+    TEXT,
+    PARAGRAPH,
+    SENTENCE,
+    TOKEN,
+    WORD,
+    DIGIT_EQUATION,
+    NUMBER,
+    MATH_OPERATOR,
+    SYMBOL;
+
+}
