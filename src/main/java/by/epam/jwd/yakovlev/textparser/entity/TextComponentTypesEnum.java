03a2830844a33520af49723a54dfac304f08482e
Alexander Yakovlev <sashayassa@gmail.com>
2020-02-19 15:09:09 +0300

    Rename TextComponentType to TextComponentTypesEnum

---
+++ src/main/java/by/epam/jwd/yakovlev/textparser/entity/TextComponentTypesEnum.java
@@ -0,0 +1,21 @@
+package by.epam.jwd.yakovlev.textparser.entity;
+
+public enum TextComponentTypesEnum {
+
+    TEXT,
+    RAW_TEXT,
+    PARAGRAPH,
+    RAW_PARAGRAPH,
+    SENTENCE,
+    RAW_SENTENCE,
+    TOKEN,
+    RAW_TAKEN,
+    WORD,
+    RAW_WORD,
+    EQUATION,
+    RAW_EQUATION,
+    NUMBER,
+    RAW_NUMBER,
+    SYMBOL;
+
+}
